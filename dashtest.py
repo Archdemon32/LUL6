4fa6ff6086545528d1eb91727d42f48d9fb01f00
Archdemon32 <102227300+Archdemon32@users.noreply.github.com>
2022-03-24 10:22:45 +0000

    Add files via upload

---
+++ dashtest.py
@@ -0,0 +1,62 @@
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+
+import pandas as pd
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+df=pd.read_excel(r'C:\Users\archd\Desktop\KEA\my_shop_data.xlsx')
+df_customers = pd.read_excel(r'C:\Users\archd\Desktop\KEA\my_shop_data.xlsx', sheet_name="customers")
+df_order = pd.read_excel(r'C:\Users\archd\Desktop\KEA\my_shop_data.xlsx', sheet_name="order")
+df_employee = pd.read_excel(r'C:\Users\archd\Desktop\KEA\my_shop_data.xlsx', sheet_name="employee")
+df_products = pd.read_excel(r'C:\Users\archd\Desktop\KEA\my_shop_data.xlsx', sheet_name="products")
+
+def get_data():
+    df_employee['Employee_Names'] = df_employee['firstname'] + ' ' + df_employee['lastname']
+    df_order['Sales'] = df_order['unitprice'] * df_order['quantity']
+
+    order = pd.merge(df_order, df_products, on='product_id')
+    order = pd.merge(order, df_employee, on='employee_id')
+    order = pd.merge(order, df_customers, on='customer_id')
+
+    return order
+
+order = get_data()
+
+# fig_product=px.bar(order,
+#     x='productname', y='Sales',
+#     color='type', title='Sales by product',
+#     labels={'Sales':'Total Sales', 'productname':'Products', 'type':'Product Type'})
+# fig_product.show()
+
+app = Dash(__name__)
+
+
+app.layout = html.Div([
+    html.H4('Sales by Employees'),
+    dcc.Dropdown(
+        id="dropdown",
+        options=["Luxuary", 'Standard'],
+        value="Standard",
+        clearable=False,
+    ),
+    dcc.Graph(id="graph"),
+])
+
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("dropdown", "value"))
+def update_bar_chart(day):
+    mask = df["type"] == type
+    fig_employee = px.bar(order,
+        x='Employee_Names', y=['Sales','orderdate'],
+        color='type', title='Sales by Employee',
+        labels={'Sales':'Total Sales', 'Employee_Names':'Employee', 'type':'Product Type'})
+    return fig
+
+
+app.run_server(debug=True)
